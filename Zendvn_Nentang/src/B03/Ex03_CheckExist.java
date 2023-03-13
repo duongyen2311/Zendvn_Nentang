63b3eb683ab5f1cb01dda5fb6170eea495303b3b
Yen <duongyen2311@gmail.com>
2023-03-13 16:13:17 +0700

    B03: Done

--- Zendvn_Nentang/src/B03/Ex03_CheckExist.java
+++ Zendvn_Nentang/src/B03/Ex03_CheckExist.java
@@ -4,21 +4,15 @@ public class Ex03_CheckExist {
 
 	public static void main(String[] args) {
 		int[] arrInt = { 0, 2, 3, 5, 6 };
-		int number = 2;
+		int number = 4;
 		String isExist = "Khong ton tai";
-		boolean flagCheckExist = false;
-		// Can them break
-		// Neu toi uu khi co nhieu cong viec sau khi thoa man dieu kien: dung flag
+
 		for (int element : arrInt) {
-			if (element == number) {
-				flagCheckExist = true;
-				break;
-			}
-		}
-		if (flagCheckExist == true) {
-			isExist = "Ton tai";
-			// CV => 50 dong code
+			if (element == number)
+				isExist = "Ton tai";
+
 		}
 		System.out.println(isExist);
 	}
+
 }
--- Zendvn_Nentang/src/B03/Ex04_CheckExistBySum.java
+++ Zendvn_Nentang/src/B03/Ex04_CheckExistBySum.java
@@ -4,19 +4,14 @@ public class Ex04_CheckExistBySum {
 
 	public static void main(String[] args) {
 		int[] arrInt = { 1, 2, 4, 3, 2 };
-		int number = 8;
+		int number = 5;
 		int length = arrInt.length;
-		String notice = "Failed";
-		boolean isSuccess = false;
+		String notice = "";
 
 		for (int i = 0; i < length - 1; i++) {
-			if (arrInt[i] + arrInt[i + 1] == number) {
-				isSuccess = true;
-				break;
-			}
-		}
-		if (isSuccess == true) {
-			notice = "Success";
+			if (arrInt[i] + arrInt[i + 1] == 5)
+				notice = "Success";
+
 		}
 		System.out.println(notice);
 	}
