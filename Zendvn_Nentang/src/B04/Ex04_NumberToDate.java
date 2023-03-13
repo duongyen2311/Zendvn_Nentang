a29bbad9711c72685787d47c960e68ccc087caa7
Yen <duongyen2311@gmail.com>
2023-03-13 23:42:30 +0700

    B04: Done

--- Zendvn_Nentang/src/B04/Ex04_NumberToDate.java
+++ Zendvn_Nentang/src/B04/Ex04_NumberToDate.java
@@ -3,27 +3,23 @@ package B04;
 public class Ex04_NumberToDate {
 
 	public static void main(String[] args) {
-		int yourNumber = 20;
-		if (yourNumber >= 12 && yourNumber <= 18 || yourNumber >= 22 && yourNumber <= 28) {
-			checkNumber(yourNumber);
-		} else
-			System.out.println("Khong the giai ma");
-	}
-
-	public static void checkNumber(int yourNumber) {
-		String stringNumber = yourNumber + "";
-		int firstNumber = Integer.parseInt(stringNumber.charAt(0) + "");
-		int secondNumber = Integer.parseInt(stringNumber.charAt(1) + "");
+		int yourNumber = 25;
+		int firstNumber = yourNumber / 10;
+		int secondNumber = yourNumber % 10;
+		String result = "Khong the giai ma";
 		// firstNumber = 1
-		String[] arrEnglish = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+		String[] arrEnglish = { "0", "1", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+				"Sunday" };
 		// firstNumber = 2
-		String[] arrVietnamese = { "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay", "Chu nhat" };
-		String result = "";
-		if (firstNumber == 1) {
-			result = arrEnglish[secondNumber - 2];
-		} else
-			result = arrVietnamese[secondNumber - 2];
-		System.out.println(result);
+		String[] arrVietnamese = { "0", "1", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay",
+				"Chu nhat" };
 
+		if (firstNumber >= 1 && firstNumber <= 2 && secondNumber >= 2 && secondNumber <= 8) {
+			if (firstNumber == 1) {
+				result = arrEnglish[secondNumber];
+			} else
+				result = arrVietnamese[secondNumber];
+		}
+		System.out.println(result);
 	}
 }
