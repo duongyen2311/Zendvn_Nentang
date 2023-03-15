f085f047c4d789aecc5b3120c540163a9c482070
duonghaiyen <duongyen2311@gmail.com>
2023-03-15 10:57:58 +0700

    B05: Reviewing

--- Zendvn_Nentang/src/B05/Ex04_FileInfo.java
+++ Zendvn_Nentang/src/B05/Ex04_FileInfo.java
@@ -4,9 +4,18 @@ public class Ex04_FileInfo {
 
 	public static void main(String[] args) {
 		String str = "D:/GoogleDrive/Doing/__psd/luutruonghailan/abc.php";
-		
-		
-		
-	}
+		// Lay abc.php
+		String[] arrStr1 = str.split("/");
+		int length = arrStr1.length - 1;
+		String fileFullName = arrStr1[length];
 
+		// Lay name + extension
+		String[] arrStr2 = fileFullName.split("\\.");
+		String name = arrStr2[0];
+		String extension = arrStr2[1];
+
+		System.out.println("Name: " + name);
+		System.out.println("Extension: " + extension);
+
+	}
 }
