ae76b8aab0ff6bfa80347b20f7f4a797a59fbe9c
Yen <duongyen2311@gmail.com>
2023-03-16 21:31:25 +0700

    B08: Processing (repush)

--- Zendvn_Nentang/src/B08/Ex01_MaxTime.java
+++ Zendvn_Nentang/src/B08/Ex01_MaxTime.java
@@ -3,7 +3,13 @@ package B08;
 public class Ex01_MaxTime {
 
 	public static void main(String[] args) {
+		// ab java 20
 
+		String inputId = "abw=java;def=android;nfv=php";
+		String inputTime = "abw=20;nfv=140;def=90";
+
+		// Task thong tin khoa hoc co thoi luong video nhieu nhat
+		// nfv - php -140
 	}
 
 }
