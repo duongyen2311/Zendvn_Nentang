5f86628035f84c4017ca6cc7dc36e3048b35ec50
Yen <duongyen2311@gmail.com>
2023-03-20 10:06:15 +0700

    B08: Done

--- Zendvn_Nentang/src/B08/Ex01_MaxTime.java
+++ Zendvn_Nentang/src/B08/Ex01_MaxTime.java
@@ -3,13 +3,29 @@ package B08;
 public class Ex01_MaxTime {
 
 	public static void main(String[] args) {
-		// ab java 20
+		String inputId = "abc=java;def=android;nfv=php";
+		String inputTime = "abc=20;nfv=1400;def=90";
+		String[] inputTimeArr = inputTime.split("[=;]");
+		int maxTime = Integer.parseInt(inputTimeArr[1]);
+		String id = "";
+		String course = "";
 
-		String inputId = "abw=java;def=android;nfv=php";
-		String inputTime = "abw=20;nfv=140;def=90";
-
-		// Task thong tin khoa hoc co thoi luong video nhieu nhat
-		// nfv - php -140
+		for (int i = 1; i < inputTimeArr.length; i += 2) {
+			int elementTime = Integer.parseInt(inputTimeArr[i]);
+			if (elementTime > maxTime) {
+				maxTime = elementTime;
+				id = inputTimeArr[i - 1];
+			}
+			
+		}
+		String[] inputIdArr = inputId.split("[=;]");
+		for (int j = 0; j < inputTimeArr.length; j += 2) {
+			if (inputIdArr[j].equalsIgnoreCase(id)) {
+				course = inputIdArr[j + 1];
+				break;
+			}
+		}
+		System.out.printf("Khoa hoc co thoi luong video nhieu nhat: %s - %s - %d", id, course, maxTime);
 	}
 
 }
